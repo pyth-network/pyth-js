f1f53d2bc682bb73eb85f79e36334a330182b335
Ali Behjati <bahjatia@gmail.com>
2022-04-26 08:15:52 +0000

    Export all types from lib and sdk

--- pyth-common-js/src/index.ts
+++ pyth-common-js/src/index.ts
@@ -1,4 +1,13 @@
 export {
+  DurationInMs,
   PriceServiceConnection,
   PriceServiceConnectionConfig,
 } from "./PriceServiceConnection";
+
+export {
+  HexString,
+  PriceFeed,
+  Price,
+  PriceStatus,
+  UnixTimestamp,
+} from "@pythnetwork/pyth-sdk-js";
