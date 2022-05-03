e25778fe3b9e850819ba230062b006310b987092
Ali Behjati <bahjatia@gmail.com>
2022-05-03 15:37:24 +0000

    Update docs + small refactor

--- pyth-terra-js/src/TerraPriceServiceConnection.ts
+++ pyth-terra-js/src/TerraPriceServiceConnection.ts
@@ -4,14 +4,22 @@ import { MsgExecuteContract } from "@terra-money/terra.js"
 export class TerraPriceServiceConnection extends PriceServiceConnection {
     /**
      * Create Terra Messages for updating given price feeds.
+     * This message can be included alongside other messages in a single transaction.
      * 
-     * @param priceId: List of id of the price feeds as an array of Hex Strings without leading 0x.
+     * Example usage:
+     * ```typescript
+     * const pythContractAddr = CONTRACT_ADDR['testnet'];
+     * const pythMsgs = await connection.getPythPriceUpdateMessage(priceIds, pythContractAddr, wallet.key.accAddress);
+     * const tx = await wallet.createAndSignTx({ msgs: [...pythMsgs, otherMsg, anotherMsg] });
+     * ```
+     * 
+     * @param priceIds: List of id of the price feeds as an array of Hex Strings without leading 0x.
      * @param pythContractAddr: Pyth contract address.
      * @param senderAddr: Sender address of the message. Sender should sign the message afterwards. 
      * @returns A Terra Message that can be included in a transaction to update the given price feed.
      */
-    async getPythPriceUpdateMessage(priceId: HexString[], pythContractAddr: string, senderAddr: string): Promise<MsgExecuteContract[]> {
-        const latestVaas = await this.getLatestVaaBytes(priceId);
+    async getPythPriceUpdateMessage(priceIds: HexString[], pythContractAddr: string, senderAddr: string): Promise<MsgExecuteContract[]> {
+        const latestVaas = await this.getLatestVaaBytes(priceIds);
         return latestVaas.map(vaa => new MsgExecuteContract(
             senderAddr,
             pythContractAddr,
--- pyth-terra-js/src/examples/TerraPriceServiceClient.ts
+++ pyth-terra-js/src/examples/TerraPriceServiceClient.ts
@@ -11,7 +11,7 @@ const argv = yargs(hideBin(process.argv))
   })
   .option("price-id", {
     description: "Price id (in hex without leading 0x) to fetch" +
-    ", you can provide more than one price id. e.g: f9c0172ba10dfa4d19088d...",
+    ", you can provide more than one price id by passing multiple --price-id. e.g: f9c0172ba10dfa4d19088d...",
     type: "array",
     required: true,
   })
