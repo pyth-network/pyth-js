4b78c8c6fc96d3156e03b05eb1827c41f5d4e2c8
Ali Behjati <bahjatia@gmail.com>
2022-06-20 10:21:15 +0200

    Add aurora_testnet (#13)

--- pyth-evm-js/src/index.ts
+++ pyth-evm-js/src/index.ts
@@ -17,4 +17,5 @@ export const CONTRACT_ADDR: Record<string, string> = {
   ropsten: "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C",
   goerli: "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C",
   mumbai: "0xff1a0f4744e8582DF1aE09D5611b887B6a12925C",
+  aurora_testnet: "0x4305FB66699C3B2702D4d05CF36551390A4c69C6",
 };
