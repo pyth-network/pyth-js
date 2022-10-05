c25bcbdc97a0878d865cf2b573f021aa938c33b3
Guillermo Bescos Alapont <gbescos@stanford.edu>
2022-10-05 12:37:21 -0500

    Reset example code

--- pyth-aptos-js/src/examples/AptosRelay.ts
+++ pyth-aptos-js/src/examples/AptosRelay.ts
@@ -1,5 +1,6 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
+
 import { AptosPriceServiceConnection } from "../index";
 import { AptosAccount, AptosClient, TxnBuilderTypes } from "aptos";
 
